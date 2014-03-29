f593acf50df4116c8589d7087930e761e72e49c7
Eric Ho <eric.ho@yale.edu>
2014-03-29 14:04:57 -0400

    Added in removal from the panlist as well as final tweaks

--- app/views.py
+++ app/views.py
@@ -10,13 +10,13 @@ TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
 client = TwilioRestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 IMSECS = {
-    "+15038939333": "Eric",
     "+15038872891": "Eric",
+    "+13037178692": "Maren",
+    "+13048905251": "Connor",
+    "+17082978240": "Julianne",
+    "+16107375637": "Claire",
+    "+16086954412": "Fabi",
 }
-
-#TO DO LIST
-#2. Front end - be able to join the panlist online
-#1. Heroku
 
 @app.route('/')
 @app.route('/index')
@@ -29,27 +29,33 @@ def text_request():
     messages = client.messages.list()
     message = messages[0].body
 
-    if from_number in IMSECS:
-        for user in Panlist.query.filter(Panlist.name=='textlist').first().users:
-            client.messages.create(to=user.number, from_="+12038506957", body=IMSECS[from_number]+ ': ' + message)
-        returnmessage = 'Thanks for sending that mass text ' + IMSECS[from_number]
-    #if the number is not an IM sec number
-    else:
-        #if it's in there, just send a text to us with what they want to say
-        if User.query.filter(User.number==from_number).first() is not None:
-            client.messages.create(to="+15038872891", from_="+12038506957", body=message)
-            #change the 'to' category to the new groupme number
-            returnmessage = 'Thanks for your input!'
-        #add them to the panlist
-        else:
-            p = Panlist.query.filter(Panlist.name=='textlist').first()
-            u = User(number=from_number,panlist=p)
-            db.session.add(u)
+    if message.strip().lower() == 'remove':
+        u = User.query.filter(User.number==from_number).first()
+        if u is not None:
+            db.session.delete(u)
             db.session.commit()
+        returnmessage = 'You have successfully been removed from the panlist'
+    else:
+        #if it's an IM sec
+        if from_number in IMSECS:
+            for user in Panlist.query.filter(Panlist.name=='textlist').first().users:
+                client.messages.create(to=user.number, from_="+12038506957", body=IMSECS[from_number]+ ': ' + message)
+            returnmessage = 'Thanks for sending that mass text ' + IMSECS[from_number]
+        #if the number is not an IM sec number
+        else:
+            #if it's in there, just send a text to us with what they want to say
+            if User.query.filter(User.number==from_number).first() is not None:
+                for num in IMSECS:
+                    client.messages.create(to=num, from_="+12038506957", body=message)
+                returnmessage = 'The IM secs have received your message and thank you for your support.'
+            #add them to the panlist
+            else:
+                p = Panlist.query.filter(Panlist.name=='textlist').first()
+                u = User(number=from_number,panlist=p)
+                db.session.add(u)
+                db.session.commit()
 
-            returnmessage = 'Thanks for joining the DAVENPORT TYNG CUP panlist! \
-            Remember that you are anonymous if you respond to this number, so be \
-            sure to leave your name so we know who you are! GO DPORT!'
+                returnmessage = 'Thanks for joining the DAVENPORT TYNG CUP panlist! This is our year to win it all, and this panlist will be used primarily as an emergency if we do not have enough players for a particular sport. Text the word "remove" if you want to be removed from the list. GO DPORT!'
 
     resp = twilio.twiml.Response()
     resp.sms(returnmessage)
--- initpanlist.py
+++ initpanlist.py
@@ -1,10 +1,24 @@
 from app import app
 from app.models import *
 
+INITLIST = {
+    "+15038872891": "Eric",
+    "+13037178692": "Maren",
+    "+13048905251": "Connor",
+    "+17082978240": "Julianne",
+    "+16107375637": "Claire",
+    "+16086954412": "Fabi",
+}
+
 db.drop_all()
 db.create_all()
 
 p = Panlist(name='textlist')
 db.session.add(p)
+
+for num in INITLIST:
+	u = User(number=num,panlist=p)
+	db.session.add(u)
+
 db.session.commit()
 
