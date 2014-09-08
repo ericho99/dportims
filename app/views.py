255f07af39c95095b5a6c6400b269cbbebc8eda4
Eric Ho <eric.ho@yale.edu>
2014-09-08 13:17:51 -0400

    added support for contact, myims, and rsvp

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from app import app
 from app.models import *
 from app.main import *
-from flask import render_template
+from flask import render_template, url_for
 from flask import Flask, request, redirect
 import twilio.twiml
 
@@ -10,12 +10,29 @@ import twilio.twiml
 def index():
     return render_template('page.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/myims')
+def myims():
+    player_id = 1
+    game_list = Attendance.query.filter(Attendance.player_id==player_id)
+    mygames = []
+    for g in game_list:
+        mygames.append(Game.query.get(g.game_id))
+    return render_template('myims.html', mygames=mygames)
+
 @app.route('/sports')
 @app.route('/sports/<int:sport>')
-def sports(sport=1):
+def sports(sport):
     games = Game.query.filter(Game.sport==sport)
     player_id = 1
-    return render_template('sport.html', sport=sport, games=games, player_id=player_id)
+    player_att = Attendance.query.filter(Attendance.player_id==player_id)
+    game_list = []
+    for att in player_att:
+        game_list.append(att.game_id)
+    return render_template('sport.html', sport=sport, games=games, game_list=game_list)
 
 @app.route('/playerlist/<int:gameid>')
 def player_list(gameid):
@@ -28,12 +45,23 @@ def player_list(gameid):
 
 @app.route('/rsvp/<int:gameid>', methods = ['POST'])
 def rsvp(gameid):
+    sport = Game.query.filter(Game.id==gameid).first().sport
     player_id = 1
-    if Attendance.query.filter(Attendance.game_id==gameid).filter(Attendance.player_id==player_id) is None:
+    if Attendance.query.filter(Attendance.game_id==gameid).filter(Attendance.player_id==player_id).first() is None:
         a = Attendance(game_id=gameid,player_id=player_id)
         db.session.add(a)
         db.session.commit()
-    return
+    return redirect(url_for('sports',sport=sport))
+
+@app.route('/unrsvp/<int:gameid>', methods = ['POST'])
+def unrsvp(gameid):
+    sport = Game.query.filter(Game.id==gameid).first().sport
+    player_id = 1
+    a = Attendance.query.filter(Attendance.game_id==gameid).filter(Attendance.player_id==player_id).first()
+    if a is not None:
+        db.session.delete(a)
+        db.session.commit()
+    return redirect(url_for('sports',sport=sport))
 
 @app.route('/textrequest', methods = ['GET','POST'])
 def text_request():
