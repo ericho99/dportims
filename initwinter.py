6706244dba5bb76fc5a519da667e43f01fae3a1c
Eric Ho <eric.ho@yale.edu>
2015-01-03 02:02:21 -0500

    init winter games

---
+++ initwinter.py
@@ -0,0 +1,80 @@
+from app import app
+from app.models import *
+from datetime import datetime
+
+
+# a and w hoops
+g=Game(sport=18,date=datetime(2014,1,15,9,00),versus='CC',win=2)
+g=Game(sport=21,date=datetime(2014,1,15,9,00),versus='CC',win=2)
+g=Game(sport=18,date=datetime(2014,1,27,8,00),versus='SY',win=2)
+g=Game(sport=21,date=datetime(2014,1,27,8,00),versus='SY',win=2)
+g=Game(sport=18,date=datetime(2014,2,9,8,00),versus='ES',win=2)
+g=Game(sport=21,date=datetime(2014,2,9,8,00),versus='ES',win=2)
+
+# b and c hoops
+g=Game(sport=19,date=datetime(2014,1,14,9,00),versus='CC',win=2)
+g=Game(sport=20,date=datetime(2014,1,14,9,00),versus='CC',win=2)
+g=Game(sport=19,date=datetime(2014,1,28,10,00),versus='SY',win=2)
+g=Game(sport=20,date=datetime(2014,1,28,10,00),versus='SY',win=2)
+g=Game(sport=19,date=datetime(2014,2,4,8,00),versus='ES',win=2)
+g=Game(sport=20,date=datetime(2014,2,4,8,00),versus='ES',win=2)
+
+# volleyball
+g=Game(sport=14,date=datetime(2014,1,21,8,00),versus='BR',win=2)
+g=Game(sport=15,date=datetime(2014,1,21,8,00),versus='BR',win=2)
+g=Game(sport=14,date=datetime(2014,2,11,10,00),versus='SY',win=2)
+g=Game(sport=15,date=datetime(2014,2,11,10,00),versus='SY',win=2)
+g=Game(sport=14,date=datetime(2014,2,16,9,00),versus='PC',win=2)
+g=Game(sport=15,date=datetime(2014,2,16,9,00),versus='PC',win=2)
+g=Game(sport=14,date=datetime(2014,2,24,10,00),versus='TD',win=2)
+g=Game(sport=15,date=datetime(2014,2,24,10,00),versus='TD',win=2)
+g=Game(sport=14,date=datetime(2014,3,02,10,00),versus='SM',win=2)
+g=Game(sport=15,date=datetime(2014,3,02,10,00),versus='SM',win=2)
+
+# m/w squash
+g=Game(sport=16,date=datetime(2014,1,14,7,30),versus='SM',win=2)
+g=Game(sport=16,date=datetime(2014,1,15,8,15),versus='SY',win=2)
+g=Game(sport=16,date=datetime(2014,1,20,8,15),versus='BR',win=2)
+g=Game(sport=16,date=datetime(2014,1,21,9,00),versus='CC',win=2)
+g=Game(sport=16,date=datetime(2014,1,28,9,00),versus='ES',win=2)
+g=Game(sport=16,date=datetime(2014,2,03,9,00),versus='MC',win=2)
+g=Game(sport=16,date=datetime(2014,2,10,9,00),versus='TC',win=2)
+g=Game(sport=16,date=datetime(2014,2,18,7,30),versus='BK',win=2)
+g=Game(sport=16,date=datetime(2014,2,19,7,30),versus='TD',win=2)
+g=Game(sport=16,date=datetime(2014,2,25,9,00),versus='PC',win=2)
+g=Game(sport=16,date=datetime(2014,2,26,7,30),versus='JE',win=2)
+g=Game(sport=17,date=datetime(2014,1,14,7,30),versus='SM',win=2)
+g=Game(sport=17,date=datetime(2014,1,15,8,15),versus='SY',win=2)
+g=Game(sport=17,date=datetime(2014,1,20,8,15),versus='BR',win=2)
+g=Game(sport=17,date=datetime(2014,1,21,9,00),versus='CC',win=2)
+g=Game(sport=17,date=datetime(2014,1,28,9,00),versus='ES',win=2)
+g=Game(sport=17,date=datetime(2014,2,03,9,00),versus='MC',win=2)
+g=Game(sport=17,date=datetime(2014,2,10,9,00),versus='TC',win=2)
+g=Game(sport=17,date=datetime(2014,2,18,7,30),versus='BK',win=2)
+g=Game(sport=17,date=datetime(2014,2,19,7,30),versus='TD',win=2)
+g=Game(sport=17,date=datetime(2014,2,25,9,00),versus='PC',win=2)
+g=Game(sport=17,date=datetime(2014,2,26,7,30),versus='JE',win=2)
+
+# water polo
+g=Game(sport=11,date=datetime(2014,1,12,8,30),versus='TC',win=2)
+g=Game(sport=11,date=datetime(2014,1,13,8,30),versus='BK',win=2)
+g=Game(sport=11,date=datetime(2014,1,19,8,30),versus='BR',win=2)
+g=Game(sport=11,date=datetime(2014,1,20,9,30),versus='CC',win=2)
+g=Game(sport=11,date=datetime(2014,1,27,9,30),versus='PC',win=2)
+g=Game(sport=11,date=datetime(2014,2,9,9,30),versus='ES',win=2)
+g=Game(sport=11,date=datetime(2014,2,11,8,30),versus='SY',win=2)
+g=Game(sport=11,date=datetime(2014,2,17,8,30),versus='JE',win=2)
+g=Game(sport=11,date=datetime(2014,2,23,8,30),versus='TD',win=2)
+g=Game(sport=11,date=datetime(2014,2,24,8,30),versus='MC',win=2)
+g=Game(sport=11,date=datetime(2014,3,02,8,30),versus='SM',win=2)
+
+
+
+
+
+
+
+
+
+
+
